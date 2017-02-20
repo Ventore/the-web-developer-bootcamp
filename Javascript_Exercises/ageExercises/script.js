96518b8304429ced55623b2b5e55a9a4e770d459
Bohdan Basov <basovbohdan@gmail.com>
2017-02-20 10:27:15 +0200

    Update and Optimize some JS code

--- Javascript_Exercises/ageExercises/script.js
+++ Javascript_Exercises/ageExercises/script.js
@@ -1,11 +1,13 @@
-var age = prompt("What is your Age?");
+var age = Number(prompt("What is your Age?"));
 
 if(age < 0){
   alert("Age can't be negative");
+} else if (age % Math.sqrt(age) === 0) {
+  alert("Your age is a perfect square");
 } else if (age === 21) {
   alert("Happy Birthday!!");
 } else if (age % 2 !== 0) {
   alert("Your age is Odd!");
-} else if (Math.sqrt(age)% 2 === 0 || Math.sqrt(age)% 2 === 1) {
-  alert("Your age is a perfect square");
+} else {
+  alert("Your age is Even!");
 };
