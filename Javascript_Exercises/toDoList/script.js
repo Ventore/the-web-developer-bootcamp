55d108df9fc201de90003d9eb155dcc7e569b809
Bohdan Basov <basovbohdan@gmail.com>
2017-02-22 14:37:04 +0200

    Complete todos app

--- Javascript_Exercises/toDoList/script.js
+++ Javascript_Exercises/toDoList/script.js
@@ -4,11 +4,31 @@ var input = prompt("What would you like to do?");
 
 while (input !== "quit") {
   if (input === "new") {
-    var newTodo = prompt("Enter new todo");
-    todos.push(newTodo);
+    addTodo();
   } else if (input === "list") {
-    console.log(todos);
+    listTodo();
+  } else if (input === "delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do?")
 }
 console.log("Good bye");
+
+// Functions for Todo app
+function addTodo() {
+  var newTodo = prompt("Enter new todo");
+  todos.push(newTodo);
+  console.log("Added todo");
+};
+function listTodo() {
+  console.log("*************");
+  todos.forEach(function(todo, i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("*************");
+};
+function deleteTodo() {
+  var index = prompt("Enter the index of todo to delete");
+  todos.splice(index, 1);
+  console.log("Todo has been deleted");
+};
