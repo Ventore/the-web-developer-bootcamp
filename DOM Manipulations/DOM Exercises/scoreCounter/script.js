6383466ac91061fa7c77b695636c6dc04cb67a3d
Bohdan Basov <basovbohdan@gmail.com>
2017-02-24 17:23:11 +0200

    Add Dom Exercise

---
+++ DOM Manipulations/DOM Exercises/scoreCounter/script.js
@@ -0,0 +1,49 @@
+var playerOneButton = document.querySelector('#playerOne');
+var playerTwoButton = document.querySelector('#playerTwo');
+var playerOneDisplay = document.querySelector('#playerOneDisplay');
+var playerTwoDisplay = document.querySelector('#playerTwoDisplay');
+var reset = document.querySelector('#reset')
+var selector = document.querySelector('input');
+var selectorNumber = document.querySelector('p span')
+
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+var gameOver = false;
+var win = 5;
+
+playerOneButton.addEventListener("click", function() {
+  if (!gameOver) {
+    playerOneScore++;
+    if (playerOneScore === win) {
+      playerOneDisplay.classList.add("winner");
+      gameOver = true;
+    }
+    playerOneDisplay.textContent = playerOneScore;
+  }
+});
+
+playerTwoButton.addEventListener("click", function() {
+  if (!gameOver) {
+    playerTwoScore++;
+    if (playerTwoScore === win) {
+      playerTwoDisplay.classList.add("winner");
+      gameOver = true;
+    }
+    playerTwoDisplay.textContent = playerTwoScore;
+  }
+});
+
+reset.addEventListener("click", function() {
+  playerOneScore = 0;
+  playerOneDisplay.textContent = playerOneScore;
+  playerOneDisplay.classList.remove("winner");
+  playerTwoScore = 0;
+  playerTwoDisplay.textContent = playerTwoScore;
+  playerTwoDisplay.classList.remove("winner");
+  gameOver = false;
+});
+
+selector.addEventListener("change", function() {
+  selectorNumber.textContent = selector.value;
+});
